93c2750672cd37aa0a6e96e36ce7979f179239d4
Alevtin_linux_home <antonov3017@gmail.com>
2022-10-30 19:20:56 +0300

    Task 1 new solution

--- HW_5/task_1.py
+++ HW_5/task_1.py
@@ -1,24 +1,16 @@
 # 1. Напишите программу, удаляющую из текста все слова, в которых присутствуют все буквы "абв".
 
-my_text = 'НапившитеБ бА абв1  прбогрАмвму2 пролд  Бав3 текста слова выба4 qwerty содержащие "бва5"'
+my_text = 'НапившитеБ бА АбВ1  прбогрАмвму2 удаляющую из ВИаб3 текста слова, в которых выба4 присутствуют все буквы "бва5"'
 print(f'Исходный текст: {my_text}')
 
-# out_txt = ' '.join(list(filter(lambda x: ('а' or 'А') and ('б' or 'Б') and ('в' or 'В') not in x, my_text.split())))
-# print(f'Текст без слов с буквами абв : {out_txt}')
-my_text = my_text.split()
-our_list = my_text
-print(our_list)
-num = []
+our_list = my_text.split()
+words_with_letters = []
+
 for i in range(len(our_list)):
     if 'а' in our_list[i].lower():
         if 'б' in our_list[i].lower():
             if 'в' in our_list[i].lower():
-                num.append(i)
-
-print(num)
-for i in range(len(num)):
-    print(i)
-    our_list.remove(our_list[num[i]])
-
-print(our_list)
-
+                words_with_letters.append(our_list[i])
+print()
+new_list = ' '.join((filter(lambda s: s not in words_with_letters, my_text.split())))
+print(f'Текст без слов с буквами абв или АБВ в любом порядке : \n{new_list}')
