732d67992d5619a4626f515ffc194c26548e7c33
Alevtin <antonov3017@gmail.com>
2022-11-30 15:58:20 +0300

    Added 2nd variant test.py

--- HW_10/func_polynom.py
+++ HW_10/func_polynom.py
@@ -4,6 +4,7 @@ from sympy import Symbol, collect
 
 def convert(item):
     item = re.sub(r'(\d)(x)', r'\1*\2', item)
+    # item = item.replace('X', 'x')
     item = item.replace('^', '**')
     item = item[:-2:]
     return item
@@ -19,3 +20,7 @@ def polynom_sum(polynom_1, polynom_2):
 
     print(f'Сумма многочленов {result=}')
     return result
+
+print(convert('x^2-1=0'))
+print(convert('x^2+1=0'))
+polynom_sum()
